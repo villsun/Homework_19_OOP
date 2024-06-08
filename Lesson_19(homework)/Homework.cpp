46536225ef154b0936799f82de30e894d317a580
villsun <lonukkola@gmail.com>
2024-06-08 23:02:27 +0200

    Add class smart_point_unique

--- Lesson_19(homework)/Homework.cpp
+++ Lesson_19(homework)/Homework.cpp
@@ -2,9 +2,50 @@
 
 using namespace std;
 
+template <typename T>
+class smart_point_unique
+{
+private:
+	T *ptr;
+	T operator = (const smart_point_unique<T> obj) = delete;
+public:
+	smart_point_unique(T* ptr = nullptr) : ptr(ptr) {}
+
+	smart_point_unique(smart_point_unique<T>&& obj)
+	{
+		ptr = obj.ptr;
+		obj.ptr = nullptr;
+	}
+
+	T& operator*()
+	{
+		if (ptr == nullptr)
+		{
+			exit(-1);
+		}
+		return *ptr;
+	}
+
+	~smart_point_unique()
+	{
+		if (ptr != nullptr)
+		{
+			delete ptr;
+		}
+	}
+};
+
 int main()
 {
+	smart_point_unique<int> ptr_1(new int(100));
+	*ptr_1 = 200;
+	cout << *ptr_1 << endl;
 
+	/*smart_point_unique<int> ptr_2;
+	ptr_2 = ptr_1;
+	cout << *ptr_2 << endl;*/
 
+	smart_point_unique<int> ptr_2(move(ptr_1));
+	cout << *ptr_2 << endl; 
 	return 0;
 }
