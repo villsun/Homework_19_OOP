c805b47780ec59b42bbb0b6cce368231fe44b643
villsun <lonukkola@gmail.com>
2024-06-08 23:27:44 +0200

    Add class smart_point_shared

--- Lesson_19(homework)/Homework.cpp
+++ Lesson_19(homework)/Homework.cpp
@@ -7,7 +7,7 @@ class smart_point_unique
 {
 private:
 	T *ptr;
-	T operator = (const smart_point_unique<T> obj) = delete;
+	/*T operator = (const smart_point_unique<T> obj) = delete;*/
 public:
 	smart_point_unique(T* ptr = nullptr) : ptr(ptr) {}
 
@@ -35,17 +35,71 @@ public:
 	}
 };
 
+template <typename T>
+class smart_point_shared
+{
+private:
+	T* ptr;
+	int* counter;
+
+public:
+	smart_point_shared(T* ptr = nullptr) : ptr(ptr), counter(new int(1)) {}
+
+	smart_point_shared<T>& operator=(const smart_point_shared<T>& other)
+	{
+		if (this == &other)
+			return *this;
+
+		if (--(*counter) == 0)
+		{
+			delete ptr;
+			delete counter;
+		}
+
+		ptr = other.ptr;
+		counter = other.counter;
+		(*counter)++;
+		return *this;
+	}
+
+	T& operator*()
+	{
+		if (ptr == nullptr)
+		{
+			exit(-1);
+		}
+		return *ptr;
+	}
+
+	~smart_point_shared()
+	{
+		if (--(*counter) == 0)
+		{
+			delete ptr;
+			delete counter;
+		}
+	}
+};
+
 int main()
 {
-	smart_point_unique<int> ptr_1(new int(100));
-	*ptr_1 = 200;
+	//smart_point_unique<int> ptr_1(new int(100));
+	//*ptr_1 = 200;
+	//cout << *ptr_1 << endl;
+
+	///*smart_point_unique<int> ptr_2;
+	//ptr_2 = ptr_1;
+	//cout << *ptr_2 << endl;*/
+
+	//smart_point_unique<int> ptr_2(move(ptr_1));
+	//cout << *ptr_2 << endl; 
+
+	smart_point_shared<int> ptr_1(new int(1));
 	cout << *ptr_1 << endl;
+	*ptr_1 = 200;
 
-	/*smart_point_unique<int> ptr_2;
-	ptr_2 = ptr_1;
-	cout << *ptr_2 << endl;*/
+	smart_point_shared<int> ptr_2 = ptr_1;
+	cout << *ptr_2 << endl;
 
-	smart_point_unique<int> ptr_2(move(ptr_1));
-	cout << *ptr_2 << endl; 
 	return 0;
 }
