279e32b191397029f5f4397b30bc4e0b24433cb7
villsun <lonukkola@gmail.com>
2024-06-08 23:37:03 +0200

    _

--- Lesson_19(homework)/Homework.cpp
+++ Lesson_19(homework)/Homework.cpp
@@ -7,7 +7,7 @@ class smart_point_unique
 {
 private:
 	T *ptr;
-	/*T operator = (const smart_point_unique<T> obj) = delete;*/
+	T operator = (const smart_point_unique<T> obj) = delete;
 public:
 	smart_point_unique(T* ptr = nullptr) : ptr(ptr) {}
 
@@ -94,12 +94,12 @@ int main()
 	//smart_point_unique<int> ptr_2(move(ptr_1));
 	//cout << *ptr_2 << endl; 
 
-	smart_point_shared<int> ptr_1(new int(1));
+	/*smart_point_shared<int> ptr_1(new int(1));
 	cout << *ptr_1 << endl;
 	*ptr_1 = 200;
 
 	smart_point_shared<int> ptr_2 = ptr_1;
-	cout << *ptr_2 << endl;
+	cout << *ptr_2 << endl;*/
 
 	return 0;
 }
